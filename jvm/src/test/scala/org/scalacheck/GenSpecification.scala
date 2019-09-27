4a1ea26e3d84470ca1a2a9929d3386582f6f535f
Aaron S. Hawley <aaron.s.hawley@gmail.com>
2019-09-27 07:06:04 -0400

    Test Gen.choose version of posNum and negNum

--- jvm/src/test/scala/org/scalacheck/GenSpecification.scala
+++ jvm/src/test/scala/org/scalacheck/GenSpecification.scala
@@ -30,6 +30,12 @@ object GenSpecification extends Properties("Gen") with GenSpecificationVersionSp
   property("negNum[Int]") =
     Prop.forAll(Gen.negNum[Int]) { n => n < 0 }
 
+  property("posNum[Long]") =
+    Prop.forAll(Gen.posNum[Long]) { n => n > 0L }
+
+  property("negNum[Long]") =
+    Prop.forAll(Gen.negNum[Long]) { n => n < 0L }
+
   property("posNum[Float]") =
     Prop.forAll(Gen.posNum[Float]) { n => n > 0.0 }
 
@@ -39,4 +45,21 @@ object GenSpecification extends Properties("Gen") with GenSpecificationVersionSp
   property("posNum[Double] <= 1.0d") = // #439
     Prop.forAll(Gen.resize(1, Gen.posNum[Double])) { _ <= 1.0d }
 
+  property("choose(1, Int.MaxValue)") =
+    Prop.forAll(Gen.choose(1, Int.MaxValue)) { n => n > 0 }
+
+  property("choose(Int.MinValue, -1)") =
+    Prop.forAll(Gen.choose(Int.MinValue, -1)) { n => n < 0 }
+
+  property("choose(1L, Long.MaxValue)") =
+    Prop.forAll(Gen.choose(1L, Long.MaxValue)) { n => n > 0L }
+
+  property("choose(Long.MinValue, -1L)") =
+    Prop.forAll(Gen.choose(Long.MinValue, -1L)) { n => n < 0L }
+
+  property("choose(1.0, Float.MaxValue)") =
+    Prop.forAll(Gen.choose(1.0, Float.MaxValue)) { n => n > 0.0 }
+
+  property("choose(Float.MinValue, -1.0)") =
+    Prop.forAll(Gen.choose(Float.MinValue, -1.0)) { n => n < 0.0 }
 }
